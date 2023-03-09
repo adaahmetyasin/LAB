5b9d56bdf72540dcc899915b7b9293a2871acd25
adaahmetyasin <ahmetyasin.ada@ogr.iuc.edu.tr>
2023-03-09 14:43:53 +0300

    Singleton Design Pattern

---
+++ Singleton Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeachaer = Singleton.GetInstance;
+            fromTeachaer.PrintDetails("From Teacher");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+
+            Console.ReadLine();
+        }
+    }
+}
