09b53c64e74633db8d2e97bce6699a0c612de05b
adaahmetyasin <ahmetyasin.ada@ogr.iuc.edu.tr>
2023-03-11 16:33:06 +0300

    Builder design pattern

---
+++ Builder/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+namespace Builder{
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+        
+        // A fresh builder instance should contain a blank product object, which
+        // is used in further assembly.
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+        
+        public void Reset()
+        {
+            this._product = new Product();
+        }
+        
+        // All production steps work with the same product instance.
+        public void BuildPartA()
+        {
+            this._product.Add("PartA1");
+        }
+        
+        public void BuildPartB()
+        {
+            this._product.Add("PartB1");
+        }
+        
+        public void BuildPartC()
+        {
+            this._product.Add("PartC1");
+        }
+        public Product GetProduct()
+        {
+            Product result = this._product;
+
+            this.Reset();
+
+            return result;
+        }
+    }
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+        
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+        
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            str = str.Remove(str.Length - 2); // removing last ",c"
+
+            return "Product parts: " + str + "\n";
+        }
+    }
+    public class Director
+    {
+        private IBuilder _builder;
+        
+        public IBuilder Builder
+        {
+            set { _builder = value; } 
+        }
+        
+        // The Director can construct several product variations using the same
+        // building steps.
+        public void BuildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+        
+        public void BuildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code creates a builder object, passes it to the
+            // director and then initiates the construction process. The end
+            // result is retrieved from the builder object.
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+            
+            Console.WriteLine("Standard basic product:");
+            director.BuildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.BuildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Remember, the Builder pattern can be used without a Director
+            // class.
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
+        }
+    }
+}
+
+
