05ca02b0a215865e469b072fec482b3f78a8ce83
adaahmetyasin <ahmetyasin.ada@ogr.iuc.edu.tr>
2023-03-18 00:02:43 +0300

    Decorator Pattern

---
+++ Decorator Pattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace DecoratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPizza pizza = new Pizza();
+            Console.WriteLine(pizza.GetPizzaType());
+
+            pizza = new PizzaWithCheese(pizza);
+            Console.WriteLine(pizza.GetPizzaType());
+
+            pizza = new PizzaWithCheese(pizza);
+            Console.WriteLine(pizza.GetPizzaType());
+
+            pizza = new PizzaWithTomatoSauce(pizza);
+            Console.WriteLine(pizza.GetPizzaType());
+
+            Console.ReadKey();
+        }
+    }
+    // base interface
+    interface IPizza{
+        string GetPizzaType();
+    }
+
+    // concrete implementation
+    class Pizza : IPizza{
+        public string GetPizzaType(){
+            return "This is a normal pizza";
+        }
+    }
+
+    // base decorator
+    class PizzaDecorator : IPizza{
+        protected IPizza _pizza;
+        public PizzaDecorator(IPizza pizza){
+            _pizza = pizza;
+        }
+
+        public virtual string GetPizzaType(){
+            return _pizza.GetPizzaType();
+        }
+    }
+
+    // concrete decorator
+    class PizzaWithCheese : PizzaDecorator{
+        public PizzaWithCheese(IPizza pizza) : base(pizza){
+        }
+
+        public override string GetPizzaType(){
+            return base.GetPizzaType() + " with cheese";
+        }
+    }
+    class PizzaWithTomatoSauce : PizzaDecorator{
+        public PizzaWithTomatoSauce(IPizza pizza) : base(pizza){
+        }
+
+        public override string GetPizzaType(){
+            return base.GetPizzaType() + " with tomato sauce";
+        }
+    }
+}
