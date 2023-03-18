17d01e53fb405272248c08002225e837aae4ba95
Yasin Ada <88513708+adaahmetyasin@users.noreply.github.com>
2023-03-18 19:44:30 +0300

    Basic Console App

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+foreach (var dir in listOfDirectories)
+{
+    Console.WriteLine(dir);
+}
+
+// Outputs:
+// stores/201
+// stores/202
+
+IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+// Outputs:
+// stores/totals.txt
+// stores/sales.json
