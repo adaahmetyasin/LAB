fc02896066133789e5fab2c68ca6467c61ff90ab
adaahmetyasin <ahmetyasin.ada@ogr.iuc.edu.tr>
2023-03-22 14:55:23 +0300

    Command Pattern

--- Command Pattern/Program.cs
+++ Command Pattern/Program.cs
@@ -2,11 +2,79 @@
 {
     static void Main(string[] args)
     {
-
+        int  sum = Calculator.calculate(10, 5, new Add());
+        int  sub = Calculator.calculate(10, 5, new Subtract());
+        int  mul = Calculator.calculate(10, 5, new Multiply());
+        int  div = Calculator.calculate(10, 5, new Divide());
+        int  pow = Calculator.calculate(10, 5, new Power());
+        int  mod = Calculator.calculate(10, 5, new Modulus());
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Sub: " + sub);
+        Console.WriteLine("Mul: " + mul);
+        Console.WriteLine("Div: " + div);
+        Console.WriteLine("Pow: " + pow);
+        Console.WriteLine("Mod: " + mod);
     }
 
     public interface Process
     {
-        ConsoleKey Key { get; }
+        int process(int num1, int num2);
     }
+
+    public class Add : Process
+    {
+        public int process(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+    public class Subtract : Process
+    {
+        public int process(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+    public class Multiply : Process
+    {
+        public int process(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+    public class Divide : Process
+    {
+        public int process(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+    public class Power : Process
+    {
+        public int process(int num1, int num2)
+        {
+            return (int)Math.Pow(num1, num2);
+        }
+    }
+
+    public class Modulus : Process
+    {
+        public int process(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+    }
+
+    public class Calculator
+    {
+        public static int calculate(int num1, int num2, Process process)
+        {
+            return process.process(num1, num2);
+        }
+    }
+
+
+
+
+
 }
